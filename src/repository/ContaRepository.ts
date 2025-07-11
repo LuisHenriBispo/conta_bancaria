14766243bc552408ee8dc9d0d81cab3aed7cc2e0
Luis Bispo <duimbispo@gmail.com>
2025-07-11 18:10:52 -0300

    Ajustes na interface ContaRepository

--- src/repository/ContaRepository.ts
+++ src/repository/ContaRepository.ts
@@ -2,14 +2,14 @@ import { Conta } from "../model/Conta";
 
 export interface ContaRepository {
     //CRUD
-    numeroConsulta(numero: number): void;
+    numeroConsulta(numeroConta: number): void;
     listarContas(): void;
     cadastrar(conta: Conta): void;
     atualizar(conta: Conta): void;
-    deletar(numero: number): void;
+    deletar(numeroConta: number): void;
     
     //METODOS
-    sacar(numero: number, valor: number): void;
-    depositar(numero: number, valor: number): void;
+    sacar(numeroConta: number, valor: number): void;
+    depositar(numeroConta: number, valor: number): void;
     transferir(numeroOrigem: number, numeroDestino: number, valorTransferencia:number): void;
 }
